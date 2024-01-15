ba03e4c6202f188ac980ae494991a890e3a31b3c
Mateus Paixão <mateus.borges.paixao@gmail.com>
2024-01-15 20:22:30 -0300

    feat: formularios 2, integração com o backend e tag select e texarea

---
+++ aula-4/formularios-2/integrar-backend/action.php
@@ -0,0 +1,14 @@
+<?php 
+
+    echo $_POST['nome'];
+    echo '<br/>';
+    echo $_POST['idade'];
+    echo '<br/>';
+    echo $_POST['senha'];
+
+    // Código backend, ex enviar para o e-mail, salva no banco de dados, etc... 
+
+    echo '<br/>';
+    echo '<a href="index.html">Voltar</a>';
+
+?>
